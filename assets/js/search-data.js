8660f6137eb1bf1bd19bb7f0e5a5836e2784cb6f
JTwiggs <167903036+JTwiggs@users.noreply.github.com>
2025-03-04 02:58:30 +0000

    Deploying to gh-pages from @ JTwiggs/JTwiggs.github.io@369e346043d4a5b896e6d684c80096963382c564 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -39,7 +39,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "Here is a more engaging take on my resume than a piece of paper. Take a look around, and reach out if you want to hear more!",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
